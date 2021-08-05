40f4f4c03f904a4aaa17888e7cb85b94e13a92dd
juliasut <juliasutygina@gmail.com>
2021-08-05 04:18:25 -0600

    Add hardcoded data to test scheduler

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,64 @@
 import React, { useState } from "react";
 import "components/Application.scss";
+import Appointment from "components/Appointment";
 import DayList from "components/DayList";
+
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "4pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Palmer Sylvia",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  }
+  
+];
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -46,7 +83,12 @@ export default function Application(props) {
 /> </>}
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => {
+          return (
+            <Appointment key={appointment.id} {...appointment} />
+          )
+        })}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
