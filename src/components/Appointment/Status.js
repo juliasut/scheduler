241859164d74793a57eac62e30e87450c47fb448
juliasut <juliasutygina@gmail.com>
2021-08-05 02:40:18 -0600

    Refactor with props

--- src/components/Appointment/Status.js
+++ src/components/Appointment/Status.js
@@ -10,7 +10,9 @@ export default function Status(props) {
     src="images/status.png"
     alt="Loading"
   />
-  <h1 className="text--semi-bold">Deleting</h1>
+  <h1 className="text--semi-bold">{props.message}</h1>
 </main>
   );
 }
+// Status component only needs to accept the following props:
+// message:String eg. "Deleting"
