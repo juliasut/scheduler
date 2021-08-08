1577a79cd49e277f3729c42679cda957785b338e
juliasut <juliasutygina@gmail.com>
2021-08-08 13:07:30 -0600

    Implement getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -7,3 +7,29 @@ export function getAppointmentsForDay(state, day) {
   // loop through appointment ids array of the given day and sub it with an appointment object with the same id from state data
   return selectedDay.appointments.map(id => state.appointments[id]);
 }
+
+// This function will return an object that contains the interview data if it is passed an object that contains an interviewer (id).
+// (input)
+// interview:
+    // {
+    //  student: "Lydia Miller-Jones",
+    //  interviewer: 1
+    // }
+// (output)
+// interview:
+  // {  
+  //   student: "Lydia Miller-Jones",
+  //   interviewer: {  
+  //     id: 1,
+  //     name: "Sylvia Palmer",
+  //     avatar: "https://i.imgur.com/LpaY82x.png"
+  //   }
+  // }
+
+export function getInterview(state, interview) {
+  if (!interview) return null;
+  const id = interview.interviewer;
+  const interviewer = state.interviewers[id];
+// spread interview object and set new interviewer key -> later interviewer value overwrites the prev one
+  return { ...interview, interviewer};
+}
