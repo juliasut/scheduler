ba3a2ca7214e977277dc0042c0ba594bbe57012a
juliasut <juliasutygina@gmail.com>
2021-08-08 02:19:56 -0600

    Make axios get request and set days state

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
 import DayList from "components/DayList";
+const axios = require('axios');
 
 
 const appointments = [
@@ -36,51 +37,31 @@ const appointments = [
   
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    axios.get("/api/days").then((response) => {
+    
+      setDays([...response.data])
+    });
+  }, []);
 
   return (
     <main className="layout">
       <section className="sidebar">
         {<>
-          <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-  <DayList
-    days={days}
-    day={day}
-    setDay={setDay}
-  />
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/> </>}
+          <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler" />
+          <hr className="sidebar__separator sidebar--centered" />
+          <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={days.name}
+            setDay={setDays}
+          />
+          </nav>
+          <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
+        </>}
       </section>
       <section className="schedule">
         {appointments.map(appointment => {
