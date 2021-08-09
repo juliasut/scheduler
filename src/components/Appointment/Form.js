21b27f82adde6717762d1378e3dd5ef8e47d4cb4
juliasut <juliasutygina@gmail.com>
2021-08-09 02:11:47 -0600

    Pass interviewers prop to Form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
@@ -9,29 +8,21 @@ export default function Form(props) {
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
   // When the user clicks the Cancel button, we clear the form values
-  const reset = () => {
-    setName("");
-    setInterviewer(null);
-  };
-
-  const cancel = () => {
-    reset();
-    props.onCancel();
-  };
-
+  const reset = () => { setName(""); setInterviewer(null); };
+  const cancel = () => { reset(); props.onCancel(); };
 
   return (
     <main className="appointment__card appointment__card--create">
-  <section className="appointment__card-left">
+    <section className="appointment__card-left">
     <form autoComplete="off" onSubmit={event => event.preventDefault()}>
       <input
         className="appointment__create-input text--semi-bold"
         name="name"
         type="text"
         placeholder="Enter Student Name"
-        /*
-          This must be a controlled component
-        */
+        // a controlled component on next 2 lines
+        value={name}
+        onChange={(event) => setName(event.target.value)}
       />
     </form>
     <InterviewerList
@@ -43,22 +34,16 @@ export default function Form(props) {
   <section className="appointment__card-right">
     <section className="appointment__actions">
       <Button danger onClick={cancel}>Cancel</Button>
-      <Button confirm>Save</Button>
+      <Button confirm onClick={props.onConfirm}>Save</Button>
     </section>
   </section>
 </main>
   );
 }
 
-// As part of our Edit story, the Form component should take the following props:
-
+// the Form component should take the following props:
 // name:String
 // interviewers:Array
 // interviewer:Number
-// onSave:Function
-// onCancel:Function
-// As part of our Create story, the Form component should take the following props:
-
-// interviewers:Array
 // onSave:Function
 // onCancel:Function
