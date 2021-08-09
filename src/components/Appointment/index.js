288c75b3afe1c7baf1cc74653f5e9f0d756cd402
juliasut <juliasutygina@gmail.com>
2021-08-09 01:02:12 -0600

    Using custom hook to render modes EMPTY, SHOW, and FORM

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import './styles.scss';
+
+import useVisualMode from 'hooks/useVisualMode';
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from './Form';
+
+// modes of the Appointment to shift between
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+    );
 
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          onEdit={}
+          onDelete={}
+        />)}
+      {mode === CREATE && (
+        <Form
+          name={props.interview.student}
+          interviewers={props.interviewers}
+          interviewer={props.interview.interviewer.id}
+          onSave={}
+          onCancel={ back }
+        />)}
     </article>
   );
 }
