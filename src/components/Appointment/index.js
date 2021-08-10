db70922330caf37260f12304fc0026b8d8a05457
juliasut <juliasutygina@gmail.com>
2021-08-09 22:13:55 -0600

    Add error handling mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,54 +1,57 @@
 import React from 'react';
 import './styles.scss';
 
-import useVisualMode from 'hooks/useVisualMode';
+import Confirm from './Confirm';
+import Empty from "./Empty";
+import Error from './Error';
+import Form from './Form';
 import Header from "./Header";
 import Show from "./Show";
-import Empty from "./Empty";
-import Form from './Form';
 import Status from './Status';
-import Confirm from './Confirm';
-import Error from './Error';
+import useVisualMode from 'hooks/useVisualMode';
 
-// modes of the Appointment to shift between
-const EMPTY = "EMPTY";
-const SHOW = "SHOW";
-const CREATE = "CREATE";
-const SAVING = "SAVING";
-const DELETING = "DELETING";
+// Modes of the Appointment to shift between
 const CONFIRM = "CONFIRM";
+const CREATE = "CREATE";
+const DELETING = "DELETING";
 const EDIT = "EDIT";
+const ERROR_DELETE = "ERROR_DELETE";
+const EMPTY = "EMPTY";
+const ERROR_SAVE = "ERROR_SAVE";
+const SAVING = "SAVING";
+const SHOW = "SHOW";
 
 
 export default function Appointment(props) {
-  
+  // State hook to transition forward and backward in history
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
     );
+  
 
+  // Helper to update mode when deleting appointment
   function save(name, interviewer) {
-    
     const interview = {
       student: name,
       interviewer
     };
-    console.log("interview: ", interview);
-    console.log("props.id: ", props.id);
     transition(SAVING);
-
-    console.log("Next calling bookInterview: ", interview);
     props.bookInterview(props.id, interview)
       .then(() => transition(SHOW))
-      .catch(err => console.log(err));
+      .catch(() => transition(ERROR_SAVE, true));
   }
 
-  function deleting(id) {
+
+  // Helper to update mode when deleting appointment
+  function destroy(id) {
     transition(DELETING, true);
     props.cancelInterview(props.id)
       .then(() => transition(EMPTY))
-      // .catch(() => transition()))
+      .catch(() => transition(ERROR_DELETE, true))
   }
-  
+
+
+  // Conditional rendering based on current mode
   return (
     <article className="appointment">
       <Header time={props.time} />
@@ -63,25 +66,27 @@ export default function Appointment(props) {
       {mode === CREATE && (
         <Form
           interviewers={props.interviewers}
-          onSave={ save }
-          onCancel={ back }
+          onSave={save}
+          onCancel={back}
         />)}
       {mode === EDIT && (
         <Form
           name={props.interview.student}
           interviewers={props.interviewers}
           interviewer={props.interview.interviewer.id}
-          onSave={ save }
-          onCancel={ back }
+          onSave={save}
+          onCancel={back}
         />)}
       {mode === SAVING && <Status message="SAVING" />}
       {mode === DELETING && <Status message="DELETING" />}
       {mode === CONFIRM && (
       <Confirm
         message="Are you sure you would like to delete?"
-        onConfirm={() => deleting(props.id)}
+        onConfirm={() => destroy(props.id)}
         onCancel={ back }
       />)}
+      {mode === ERROR_SAVE && <Error message="Could not save appointment" onClose={back} />}
+      {mode === ERROR_DELETE && <Error message="Could not delete appointment" onClose={back} />}
     </article>
   );
 }
