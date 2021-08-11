b78f8d007c888288efa98590d8e2ce5b18c9cf06
juliasut <juliasutygina@gmail.com>
2021-08-10 23:12:52 -0600

    Refactor Form tests

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -61,7 +61,6 @@ describe("Form", () => {
     fireEvent.click(getByText("Save"));
 
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
-
     expect(onSave).toHaveBeenCalledTimes(1);
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
   });
@@ -86,9 +85,7 @@ describe("Form", () => {
     fireEvent.click(getByText("Cancel"));
 
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
-
     expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
-
     expect(onCancel).toHaveBeenCalledTimes(1);
   });
 });
