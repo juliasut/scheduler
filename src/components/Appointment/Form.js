8130dae0281c8a5dbc9cb518703b0b58d6aefe4b
juliasut <juliasutygina@gmail.com>
2021-08-10 22:51:39 -0600

    Add more jest tests for Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,10 +6,21 @@ export default function Form(props) {
 
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   // When the user clicks the Cancel button, we clear the form values
   const reset = () => { setName(""); setInterviewer(null); };
   const cancel = () => { reset(); props.onCancel(); };
+  
+  const validate = () => {
+    if (name === "") {
+      setError("student name cannot be blank");
+      return;
+    }
+    setError("");
+    props.onSave(name, interviewer);
+  }
+
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -26,6 +37,7 @@ export default function Form(props) {
         data-testid="student-name-input"
       />
     </form>
+    <section className="appointment__validation">{error}</section>
     <InterviewerList
       interviewers={props.interviewers} 
       value={interviewer} 
@@ -35,7 +47,7 @@ export default function Form(props) {
   <section className="appointment__card-right">
     <section className="appointment__actions">
       <Button danger onClick={cancel}>Cancel</Button>
-      <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
+      <Button confirm onClick={validate}>Save</Button>
     </section>
   </section>
 </main>
