fb0c7fa38ab55dbe6bfa81298e81c2c708266d6c
juliasut <juliasutygina@gmail.com>
2021-08-12 14:46:18 -0600

    update

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -53,52 +53,7 @@ const fixtures = {
   },
 };
 
-export default {
 
-  get: ((url) => {
-    if (url === '/api/days') {
-      return Promise.resolve({
-        status: 200,
-        statusText: 'OK',
-        data: fixtures.days,
-      });
-    }
-
-    if (url === '/api/appointments') {
-      /* Resolve appointments data */
-      return Promise.resolve({
-        status: 200,
-        statusText: 'OK',
-        data: fixtures.appointments,
-      });
-    }
-
-    if (url === "/api/interviewers") {
-      /* Resolve interviewers data */
-      return Promise.resolve({
-        status: 200,
-        statusText: "OK",
-        data: fixtures.interviewers,
-      });
-    }
-  }),
-
-  put: ((url) => {
-    return Promise.resolve({
-      status: 204,
-      statusText: "No Content",
-    });
-  }),
-
-  delete: ((url) => {
-    return Promise.resolve({
-      status: 204,
-      statusText: "No Content",
-    });
-  }),
-};
-
-/*
 export default {
   get: jest.fn(url => {
     if (url === "api/days") {
@@ -140,4 +95,3 @@ export default {
     });
   })
 };
-*/
